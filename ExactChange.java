bdd4531e216688f3581bfd19ce4f0fa4fbca9df5
Adam <adh636@gmail.com>
2016-02-14 03:05:20 -0500

    add 5 more test cases

--- ExactChange.java
+++ ExactChange.java
@@ -4,10 +4,56 @@ public class ExactChange {
 		double price = .01;
         double cash = .02;
         double[][] cid = {{.01, 0.10}, {.05, 0.0}, {.10, 0.0},
-                {.25, 0.0}, {1.200, 0.0}, {5.00, 0.00}, {10.00, 0.0},
+                {.25, 0.0}, {1.00, 0.0}, {5.00, 0.00}, {10.00, 0.0},
                 {20.00, 0.0}, {100.00, 0.00}};
 		double[][] change = calculateChange(price, cash, cid);
 		printArray(price, cash, change, cid);
+		System.out.println();
+		
+		double price2 = 19.50;
+		double cash2 = 20.00;
+		double[][] cid2 = {{.01, 1.01}, {.05, 2.05}, {.10, 3.10},
+				{.25, 4.25}, {1.00, 90.00}, {5.00, 55.00}, {10.00, 20.00},
+				{20.00, 60.00}, {100.00, 100.00}};
+		double[][] change2 = calculateChange(price2, cash2, cid2);
+		printArray(price2, cash2, change2, cid2);
+		System.out.println();
+		
+		double price3 = 3.26;
+		double cash3 = 100.00;
+		double[][] cid3 = {{.01, 1.01}, {.05, 2.05}, {.10, 3.10},
+				{.25, 4.25}, {1.00, 90.00}, {5.00, 55.00}, {10.00, 20.00},
+				{20.00, 60.00}, {100.00, 100.00}};
+		double[][] change3 = calculateChange(price3, cash3, cid3);
+		printArray(price3, cash3, change3, cid3);
+		System.out.println();
+		
+		double price4 = 19.50;
+		double cash4 = 20.00;
+		double[][] cid4 = {{.01, .01}, {.05, 0.00}, {.10, 0.00},
+				{.25, 0.00}, {1.00, 0.00}, {5.00, 0.00}, {10.00, 0.00},
+				{20.00, 0.00}, {100.00, 0.00}};
+		double[][] change4 = calculateChange(price4, cash4, cid4);
+		printArray(price4, cash4, change4, cid4);
+		System.out.println();
+		
+		double price5 = 19.50;
+		double cash5 = 20.00;
+		double[][] cid5 = {{.01, .01}, {.05, 0.00}, {.10, 0.00},
+				{.25, 0.00}, {1.00, 1.00}, {5.00, 0.00}, {10.00, 0.00},
+				{20.00, 0.00}, {100.00, 0.00}};
+		double[][] change5 = calculateChange(price5, cash5, cid5);
+		printArray(price5, cash5, change5, cid5);
+		System.out.println();
+		
+		double price6 = 19.50;
+		double cash6 = 20.00;
+		double[][] cid6 = {{.01, .50}, {.05, 0.00}, {.10, 0.00},
+				{.25, 0.00}, {1.00, 0.00}, {5.00, 0.00}, {10.00, 0.00},
+				{20.00, 0.00}, {100.00, 0.00}};
+		double[][] change6 = calculateChange(price6, cash6, cid6);
+		printArray(price6, cash6, change6, cid6);
+		System.out.println();
 	}
 	
 	// returns a 2d array of change due
